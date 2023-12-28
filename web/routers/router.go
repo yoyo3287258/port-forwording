0ff86366529296766f7f7deca172acd000bf6199
刘霖坤 <yoyo3287258@yeah.net>
2023-12-28 18:28:02 +0800

    添加内嵌式的前端文件和静态文件

--- web/routers/router.go
+++ web/routers/router.go
@@ -1,14 +1,15 @@
 package routers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func InitRouters(r *gin.Engine) {
-	r.GET("/ping", func(c *gin.Context) {
+	r.Any("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"data": "pong",
 		})
 	})
-
 	InitPortForwardRouter(r)
 
 }
--- web/server.go
+++ web/server.go
@@ -1,15 +1,42 @@
 package web
 
 import (
+	"embed"
 	"github.com/gin-gonic/gin"
+	"io"
+	"io/fs"
 	"port-forwording/web/routers"
 )
 
+//go:embed dist
+var Dist embed.FS
+
 func Run() {
 	r := gin.Default()
+	index(r)
 	routers.InitRouters(r)
 	err := r.Run(":8765")
 	if err != nil {
 		return
 	}
+}
+
+func index(r *gin.Engine) {
+	r.NoRoute(func(c *gin.Context) {
+		dist, _ := fs.Sub(Dist, "dist")
+		indexFile, err := dist.Open("index.html")
+		if err != nil {
+			panic(err.Error())
+		}
+		defer func() {
+			_ = indexFile.Close()
+		}()
+		content, _ := io.ReadAll(indexFile)
+
+		c.Header("Content-Type", "text/html")
+		c.Status(200)
+		_, _ = c.Writer.WriteString(string(content))
+		c.Writer.Flush()
+		c.Writer.WriteHeaderNow()
+	})
 }
